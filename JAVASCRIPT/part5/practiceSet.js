e684d1e92a66337371b1f48aa9d6481552af9bda
Rk4609 <rakeshjangid8742@gmail.com>
2026-07-04 16:18:31 +0530

    Add initial JavaScript file for practice set examples

---
+++ JAVASCRIPT/part5/practiceSet.js
@@ -0,0 +1,23 @@
+
+// let dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+// const car = {
+//     name:"Thar",
+//     model:2026,
+//     color:"black"
+// }
+
+// let c =car.name;
+// console.log(c);
+
+// const person = {
+//     name:"tony",
+//     age:35,
+//     city:"Russia"
+// }
+
+// console.log(person.city="New York");
+// console.log(person.country="United States");
+// console.log(person);
+
